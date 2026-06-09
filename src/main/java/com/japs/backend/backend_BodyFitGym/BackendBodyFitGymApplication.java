4c99d4c1816ee30897dfa536ce909348263355c2
Lopez Andres <lopezquintero271@gmail.com>
2026-06-08 20:38:49 -0500

    fix: add missing files from initial upload

---
+++ src/main/java/com/japs/backend/backend_BodyFitGym/BackendBodyFitGymApplication.java
@@ -0,0 +1,13 @@
+package com.japs.backend.backend_BodyFitGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBodyFitGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBodyFitGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/japs/backend/backend_BodyFitGym/BackendBodyFitGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japs.backend.backend_BodyFitGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBodyFitGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
